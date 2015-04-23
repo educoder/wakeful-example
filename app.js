f8c0add7afa5246022a8e7c06cce1680e90573f0
Armin Krauss <armin.krauss@utoronto.ca>
2015-04-22 22:11:35 -0400

    initial version. Super simple and yet real time

---
+++ app.js
@@ -0,0 +1,49 @@
+(function() {
+  "use strict";
+  var Skeletor = this.Skeletor || {};
+  this.Skeletor.Mobile = this.Skeletor.Mobile || new Skeletor.App();
+  var app = this.Skeletor.Mobile;
+
+  app.config = null;
+  app.requiredConfig = {
+    wakeful: {
+      url: 'string'
+    }
+  };
+
+  app.client = null;
+
+  app.init = function() {
+    /* CONFIG */
+    app.loadConfig('../config.json');
+    app.verifyConfig(app.config, app.requiredConfig);
+
+    app.client = new Faye.Client(app.config.wakeful.url);
+
+    app.setupClickListeners();
+
+    app.subscribeToChannel('/foo');
+  },
+
+  app.setupClickListeners = function () {
+    jQuery('#submit-message').click(function(ev) {
+      var message = jQuery('input').val();
+      // console.log(message);
+      app.client.publish('/foo', {text: message});
+    });
+  },
+
+  app.subscribeToChannel = function (streamName) {
+    var subscription = app.client.subscribe(streamName, function(message) {
+      // handle message
+      console.log(message);
+      var msgList = jQuery('#messages');
+      var msgItem = jQuery('<li>'+message.text+'</li>');
+      msgList.append(msgItem);
+    });
+
+  }
+
+  // this.Skeletor.Mobile = app;
+  this.Skeletor = Skeletor;
+}).call(this);
